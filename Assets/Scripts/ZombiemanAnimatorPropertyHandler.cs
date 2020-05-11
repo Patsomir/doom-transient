0d524344673451fd9a4334f5135e11c5c0bd0203
Patsomir <pnn246@gmail.com>
2020-05-11 22:02:24 +0300

    Implement Zombieman death
    Zombieman now plays a damage animation upon taking damage and a death
    animation upon he's health reaching zero.

---
+++ Assets/Scripts/ZombiemanAnimatorPropertyHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ZombiemanAnimatorPropertyHandler : MonoBehaviour
+{
+    private Animator animator = null;
+    private DemonStats stats = null;
+
+    private int previousHealth;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        stats = GetComponent<DemonStats>();
+        previousHealth = stats.Health;
+    }
+
+    void Update()
+    {
+        animator.SetInteger("Health", stats.Health);
+        if(stats.Health < previousHealth)
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+        previousHealth = stats.Health;
+    }
+}
