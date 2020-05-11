74cc3f8c5e783ccb94c9382b15345b5a56a1cef8
Patsomir <pnn246@gmail.com>
2020-05-11 03:34:57 +0300

    Add head animations to HUD
    The head in the HUD now has different expressions depending on the
    health of the player. It also reacts upon the player taking damage.

---
+++ Assets/Scripts/HeadUIHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HeadUIHandler : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerStats stats = null;
+
+    private Animator animator = null;
+    private int previousFrameHealth;
+    void Start()
+    {
+        if (stats == null)
+        {
+            stats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        }
+        animator = GetComponent<Animator>();
+        previousFrameHealth = stats.Health;
+    }
+
+    void Update()
+    {
+        if(stats.Health < previousFrameHealth)
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+        previousFrameHealth = stats.Health;
+        animator.SetFloat("Health", stats.Health);
+    }
+}
