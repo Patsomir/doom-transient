b92bf22ba34f7bd632af57f27cd4b42217ed6e5a
Patsomir <pnn246@gmail.com>
2020-05-13 02:06:25 +0300

    Add head smile animation
    If the player rapidly clicks the left mouse button for 2 seconds, a
    smile animation will be displayed on the HUD head.

--- Assets/Scripts/HeadUIHandler.cs
+++ Assets/Scripts/HeadUIHandler.cs
@@ -7,6 +7,15 @@ public class HeadUIHandler : MonoBehaviour
 
     private Animator animator = null;
     private int previousFrameHealth;
+
+
+    [SerializeField]
+    private float rageTimeout = 0.5f;
+
+    private float rageTime = 0.0f;
+    private float rageTimeoutTimestamp;
+
+
     void Start()
     {
         if (stats == null)
@@ -15,6 +24,7 @@ public class HeadUIHandler : MonoBehaviour
         }
         animator = GetComponent<Animator>();
         previousFrameHealth = stats.Health;
+        rageTimeoutTimestamp = Time.time;
     }
 
     void Update()
@@ -25,5 +35,44 @@ public class HeadUIHandler : MonoBehaviour
         }
         previousFrameHealth = stats.Health;
         animator.SetFloat("Health", stats.Health);
+
+        if (!HasRageTimedOut())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                IncrementRage();
+                UpdateRageTimestamp();
+            }
+        } else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                UpdateRageTimestamp();
+            }
+            ResetRage();
+        }
+
+        animator.SetFloat("Rage", rageTime);
     }
+
+    bool HasRageTimedOut()
+    {
+        return rageTimeoutTimestamp < Time.time;
+    }
+
+    void UpdateRageTimestamp()
+    {
+        rageTimeoutTimestamp = Time.time + rageTimeout;
+    }
+
+    void IncrementRage()
+    {
+        rageTime += Time.time - (rageTimeoutTimestamp - rageTimeout);
+    }
+
+    void ResetRage()
+    {
+        rageTime = 0;
+    }
+
 }
