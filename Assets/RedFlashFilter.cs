31dbc9dcccf627fa57671bf698bfb0d28eea057c
Patsomir <pnn246@gmail.com>
2020-05-12 21:44:19 +0300

    Add red flash filther
    The screen flashes red whenever the player takes damage.

---
+++ Assets/RedFlashFilter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedFlashFilter : MonoBehaviour
+{
+    [SerializeField]
+    private Material mat = null;
+
+    [SerializeField]
+    private float intensity = 0.8f;
+
+    [SerializeField]
+    private float duration = 0.1f;
+
+    [SerializeField]
+    private PlayerStats healthSource = null;
+
+    private int previousHealth;
+    private float turnOffTimestamp;
+    private bool tookDamageThisFrame;
+
+    private void Start()
+    {
+        if (healthSource == null)
+        {
+            healthSource = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        }
+        previousHealth = healthSource.Health;
+        turnOffTimestamp = Time.time;
+    }
+    private void Update()
+    {
+        if (mat != null)
+        {
+            mat.SetFloat("_Intensity", intensity);
+        }
+        if(healthSource.Health < previousHealth)
+        {
+            turnOffTimestamp = Time.time + duration;
+        }
+        previousHealth = healthSource.Health;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (mat != null && Time.time < turnOffTimestamp)
+        {
+            Graphics.Blit(source, destination, mat);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
---
+++ Assets/Shaders/RedFlash.shader
@@ -0,0 +1,49 @@
+﻿Shader "Unlit/RedFlash"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+        _Intensity("Intensity", Float) = 0.8
+    }
+        SubShader
+        {
+            Tags { "RenderType" = "Opaque" }
+            LOD 100
+
+            Pass
+            {
+                CGPROGRAM
+                #pragma vertex vert
+                #pragma fragment frag
+                #pragma multi_compile_fog
+
+                #include "UnityCG.cginc"
+
+                struct appdata
+                {
+                    float4 vertex : POSITION;
+                };
+
+                struct v2f
+                {
+                    float4 vertex : SV_POSITION;
+                };
+
+                sampler2D _MainTex;
+                float _Intensity;
+
+                v2f vert(appdata v)
+                {
+                    v2f o;
+                    o.vertex = UnityObjectToClipPos(v.vertex);
+                    return o;
+                }
+
+                fixed4 frag(v2f i) : SV_Target
+                {
+                    return float4(_Intensity, 0, 0, 1);
+                }
+                ENDCG
+            }
+        }
+}
