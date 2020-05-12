832b6985c3b012773c7345603ebfdf65e7e2336d
Patsomir <pnn246@gmail.com>
2020-05-12 05:16:06 +0300

    Improve Zombieman AI
    Zombieman now stands still while his damage animation plays.

---
+++ Assets/ZombiemanHurtAI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ZombiemanHurtAI : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.GetComponent<ZombiemanMovement>().MoveTo(animator.transform.position);
+    }
+}
